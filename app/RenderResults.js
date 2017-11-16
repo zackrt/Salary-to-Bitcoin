b48aa0e6cd08e24492f67da6204ff6c6528eb0f1
Zack Toth <zacharyrtoth@gmail.com>
2017-11-16 15:45:26 -0800

    question for connecting second API

--- app/RenderResults.js
+++ app/RenderResults.js
@@ -8,3 +8,4 @@ export function renderResults(Amount) {
     $("#conversion-results").html(`${Amount.toString()}`);
 }
 // how to render results in 
+// how to send Amount to Next page to display top 4 items they can buy with that much USD
