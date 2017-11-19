8694813913ac66556c64a6c30f58b8a5b99a1ec4
Zack Toth <zacharyrtoth@gmail.com>
2017-11-18 17:00:01 -0800

    added shopzilla endpoint

--- app/shopWithBitcoin.js
+++ app/shopWithBitcoin.js
@@ -1,1 +1,2 @@
-const Shop_URL = "http://api.walmartlabs.com/v1/search?query=ipod&format=json&apiKey="
+const Walmart_Shop_URL = "http://api.walmartlabs.com/v1/search?query=ipod&format=json&apiKey="
+const Shopzilla_URL ="http://catalog.bizrate.com/services/catalog/v1/"
