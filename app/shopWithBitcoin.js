41190bcfd6cefe95f160c0031a17a594a25d6ae5
Zack Toth <zacharyrtoth@gmail.com>
2017-11-19 10:44:30 -0800

    Added Ebay browse API

--- app/shopWithBitcoin.js
+++ app/shopWithBitcoin.js
@@ -1,2 +1,7 @@
-const Walmart_Shop_URL = "http://api.walmartlabs.com/v1/search?query=ipod&format=json&apiKey="
-const Shopzilla_URL ="http://catalog.bizrate.com/services/catalog/v1/"
+const EBAY_URL = "https://api.ebay.com/buy/browse/v1";
+//App ID (Client ID)
+application_key: zacharyt-thinkful-SBX-e5d74fc8f-ad39c919
+//Dev ID	
+devID : 3a7ae6c4-125f-461a-8569-1385701b43fd
+//Cert ID (Client Secret)
+certID:	SBX-5d74fc8f5a7a-af33-4879-b07d-0a4a
