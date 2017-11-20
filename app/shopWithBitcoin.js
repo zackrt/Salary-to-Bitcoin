94764737a8b1f1d07971f360bcd218b68be54738
Zack Toth <zacharyrtoth@gmail.com>
2017-11-19 16:00:31 -0800

    suggestions from Marius Banea

--- app/shopWithBitcoin.js
+++ app/shopWithBitcoin.js
@@ -5,3 +5,5 @@ application_key: zacharyt-thinkful-SBX-e5d74fc8f-ad39c919
 devID : 3a7ae6c4-125f-461a-8569-1385701b43fd
 //Cert ID (Client Secret)
 certID:	SBX-5d74fc8f5a7a-af33-4879-b07d-0a4a
+
+//or use temp paid email account to get bestbuy API key
