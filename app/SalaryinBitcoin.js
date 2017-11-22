0f44f8bb18da10c5fc9e729c1fbaad89a68bcc3e
Zack Toth <zacharyrtoth@gmail.com>
2017-11-22 09:32:51 -0800

    connected jobs .usa.gov api to job search

---
+++ app/SalaryinBitcoin.js
@@ -0,0 +1,28 @@
+const url =
+"https://api.usa.gov/jobs/search.json";
+
+$(document).ready(() => {
+$('#jobs').click(() => {
+  const query = $('input').val();
+  $.getJSON(url, {
+    query
+  }).then(data => {
+    console.log(data);
+    var govSalary = data;
+    for (const job of data) {
+      $('salary-results').html(`${job.position_title} ${job.maximum}`);
+    }
+    RenderJobs(data);
+  });
+})
+});
+
+function RenderJobs(){
+        return `
+        <div>
+            <h2>With this Salary, you can acquire this much Bitcoin:
+            </h2>
+           
+        </div>
+    `
+};
